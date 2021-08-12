fd9aad54d1847cfbb4f5a3e62928eebd4da59a9b
worklifesg <worklifesg@github.com>
2021-08-12 08:57:54 -0400

    FirstCommit

---
+++ Fundamentals/createscript.sh
@@ -0,0 +1,7 @@
+#Commands
+
+#echo echo "Hello ! This is my first script" > ./BashScripting/Fundamentals/createscript.sh
+#chmod 755 ./BashScripting/Fundamentals/createscript
+#./BashScripting/Fundamentals/createscript
+
+echo Hello ! This is my first script
